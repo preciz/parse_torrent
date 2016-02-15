7d343cbca18e9e050326d49012ddf6497e43ac30
Barna Kovacs <coolreef@gmail.com>
2016-02-15 17:35:43 +0100

    Decode the .torrent file and ensure it has required keys

--- lib/parse_torrent.ex
+++ lib/parse_torrent.ex
@@ -1,2 +1,28 @@
 defmodule ParseTorrent do
+  def parse(file_path) do
+    torrent = File.read!(file_path)
+
+    torrent
+    |> Bencodex.decode
+    |> torrent_valid?
+  end
+
+  defp torrent_valid?(torrent) do
+    import Map, only: [get: 2, fetch!: 2]
+
+    try do
+      torrent |> fetch!("info")
+      torrent |> get("info") |> fetch!("name")
+      torrent |> get("info") |> fetch!("piece length")
+      torrent |> get("info") |> fetch!("pieces")
+    rescue
+      _ -> raise(InvalidTorrentError)
+    end
+
+    torrent
+  end
+end
+
+defmodule InvalidTorrentError do
+  defexception message: "Missing key"
 end
