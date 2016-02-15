66622af8b2a439d7ff1d2941d3e080ee3738445d
Barna Kovacs <coolreef@gmail.com>
2016-02-15 13:08:36 +0100

    add description

--- mix.exs
+++ mix.exs
@@ -1,10 +1,15 @@
 defmodule ParseTorrent.Mixfile do
   use Mix.Project
 
+  @description """
+    Parses a torrent file and returns a map
+  """
+
   def project do
     [app: :parse_torrent,
      version: "0.0.1",
      elixir: "~> 1.2",
+     description: @description,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
