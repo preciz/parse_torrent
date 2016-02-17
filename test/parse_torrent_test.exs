2d783a856c8ad1582246a57059e45ee6053c9a82
Barna Kovacs <coolreef@gmail.com>
2016-02-17 19:49:37 +0100

    tests for basic error cases and empty string handling

--- test/parse_torrent_test.exs
+++ test/parse_torrent_test.exs
@@ -2,7 +2,28 @@ defmodule ParseTorrentTest do
   use ExUnit.Case
   doctest ParseTorrent
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "raises error when torrent file doesn't exist" do
+    assert_raise File.Error, fn ->
+      ParseTorrent.parse("not-existing.torrent")
+    end
   end
+
+  test "raises error when torrent is missing 'name' field" do
+    assert_raise InvalidTorrentError, fn ->
+      ParseTorrent.parse("test/torrents/leaves-corrupt.torrent")
+    end
+  end
+
+  test "parse torrent with empty announce-list" do
+    torrent = ParseTorrent.parse("test/torrents/leaves-empty-announce-list.torrent")
+
+    assert(torrent.announce == ["udp://tracker.publicbt.com:80/announce"])
+  end
+
+  test "parses empy url-list" do
+    torrent = ParseTorrent.parse("test/torrents/leaves-empty-url-list.torrent")
+
+    assert torrent.url_list == []
+  end
+
 end
