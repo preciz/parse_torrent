e8807a8a208c402030f1a9aa60c3548b1b3bf9ce
Barna Kovacs <coolreef@gmail.com>
2016-02-18 15:29:39 +0100

    create package function

--- mix.exs
+++ mix.exs
@@ -15,23 +15,19 @@ defmodule ParseTorrent.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     []
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [{:bencodex, "1.0.0"}]
+  end
+
+  defp package do
+    [
+      contributors: ["Barna Kovacs"],
+      licenses: ["UNLICENSE"],
+      links: %{"GitHub" => "https://github.com/preciz/parse_torrent"}
+    ]
   end
 end
