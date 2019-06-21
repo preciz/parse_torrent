76169b7b7a5f128605216b93a50df1c8bbfea4e3
preciz <barna.kovacs.codes@gmail.com>
2019-06-21 09:41:03 +0200

    Remove earmark dependency & use bencode 0.3.2

--- mix.exs
+++ mix.exs
@@ -5,15 +5,23 @@ defmodule ParseTorrent.Mixfile do
     Parses a .torrent file and returns a map
   """
 
+  @version "0.3.0"
+
   def project do
-    [app: :parse_torrent,
-     version: "0.2.0",
-     elixir: "~> 1.2",
-     description: @description,
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     package: package,
-     deps: deps]
+    [
+      app: :parse_torrent,
+      version: @version,
+      elixir: "~> 1.4",
+      description: @description,
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+
+      name: "ParseTorrent",
+      docs: docs(),
+
+      package: package(),
+    ]
   end
 
   def application do
@@ -22,9 +30,8 @@ defmodule ParseTorrent.Mixfile do
 
   defp deps do
     [
-      {:earmark, "0.2.1"},
-      {:bencode, "0.3.0"},
-      {:ex_doc, "0.11.4", only: [:dev]}
+      {:bencode, "0.3.2"},
+      {:ex_doc, "~> 0.20.2", only: :dev, runtime: false}
     ]
   end
 
@@ -33,6 +40,14 @@ defmodule ParseTorrent.Mixfile do
       maintainers: ["Barna Kovacs"],
       licenses: ["UNLICENSE"],
       links: %{"GitHub" => "https://github.com/preciz/parse_torrent"}
+    ]
+  end
+
+  def docs do
+    [
+      main: "ParseTorrent",
+      source_ref: "v#{@version}",
+      source_url: "https://github.com/preciz/parse_torrent",
     ]
   end
 end
