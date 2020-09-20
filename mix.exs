0493c17dbbfb2cef19b97e3b333c4e1298d3b735
Kian-Meng, Ang <kianmeng@cpan.org>
2020-09-20 16:17:12 +0800

    Add credo module

--- mix.exs
+++ mix.exs
@@ -30,6 +30,7 @@ defmodule ParseTorrent.Mixfile do
   defp deps do
     [
       {:bencode, "0.3.2"},
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.22.2", only: :dev, runtime: false}
     ]
   end
